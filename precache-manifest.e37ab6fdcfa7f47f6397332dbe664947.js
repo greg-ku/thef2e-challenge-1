e5757104e248ef00f191ea31f46a13c2b5afb96d
GregKu <s352806@sinyi.com.tw>
2019-07-08 16:47:12 +0800

    Updates

---
+++ precache-manifest.e37ab6fdcfa7f47f6397332dbe664947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34069d00984023f309bf0120d5f90b56",
+    "url": "/thef2e-challenge-1/index.html"
+  },
+  {
+    "revision": "1fefbb6035e9de342fc2",
+    "url": "/thef2e-challenge-1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/thef2e-challenge-1/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1fefbb6035e9de342fc2",
+    "url": "/thef2e-challenge-1/static/js/main.abc10001.chunk.js"
+  },
+  {
+    "revision": "23f86d3e5ce9e9dcd01a",
+    "url": "/thef2e-challenge-1/static/js/runtime~main.acf08efb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/thef2e-challenge-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
